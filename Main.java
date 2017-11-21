6ac6e8809c26189ce324c6f4d4155089cb277730
Phiisource <33837880+Phiisource@users.noreply.github.com>
2017-11-21 17:27:52 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+	public static void main (String[] args) {
+		/*
+		// Permet de choisir le nombre de joueurs entre 2 et 5 (en comptant l'utilisateur) :
+		int nbJoueurs;
+		Scanner sc = new Scanner(System.in); 
+		do {
+		System.out.println("Contre combien d'adversaires IA souhaitez-vous jouer ? (entre 1 et 4)");
+		nbJoueurs = sc.nextInt();
+		}
+		while (nbJoueurs > 4 || nbJoueurs < 1);
+		
+		Partie partieCourante = new Partie(nbJoueurs + 1);*/
+		
+		Partie partieCourante = new Partie(3);
+	}
+}
